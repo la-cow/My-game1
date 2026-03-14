74e50c10ff02eddfef7c3a4a2762f6cadeab9fc3
la-cow <lanew2016@gmail.com>
2026-03-14 13:44:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,17 +5,47 @@ const io = require('socket.io')(http);
 
 app.use(express.static('public'));
 
+let userCount = 0;
+// 準備一組漂亮的顏色給玩家
+const colors = ['#FF5733', '#28A745', '#007BFF', '#E83E8C', '#FD7E14', '#6F42C1', '#20C997', '#FFC107'];
+
 io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+  // 處理 j3 進入請求
+  socket.on('join_chat', () => {
+    userCount++;
+    socket.userData = {
+      id: userCount,
+      color: colors[userCount % colors.length]
+    };
+    // 回傳給該玩家專屬資訊
+    socket.emit('chat_joined', socket.userData);
   });
-  socket.on('file upload', (data) => {
-    io.emit('file upload', data);
+
+  // 轉發文字訊息
+  socket.on('chat message', (msg) => {
+    if (socket.userData) {
+      io.emit('chat message', {
+        id: socket.userData.id,
+        color: socket.userData.color,
+        text: msg
+      });
+    }
+  });
+
+  // 轉發檔案 (改為點擊下載模式)
+  socket.on('file upload', (file) => {
+    if (socket.userData) {
+      io.emit('file upload', {
+        id: socket.userData.id,
+        color: socket.userData.color,
+        fileName: file.name,
+        fileData: file.data
+      });
+    }
   });
 });
 
 const port = process.env.PORT || 3000;
 http.listen(port, () => {
-  console.log('伺服器順利啟動囉！');
+  console.log('伺服器已啟動於連接埠 ' + port);
 });
-
